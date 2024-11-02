e1f28d528df131777c2846f3aae4430633da8daf
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-11-02 16:27:13 +0530

    commit for site map

--- pandit_website/sitemaps.py
+++ pandit_website/sitemaps.py
@@ -1,10 +1,23 @@
 from django.contrib.sitemaps import Sitemap
 from services.models import Typesofpuja
-
+from django.urls import reverse
 
 class TypesofpujaSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
     def items(self):
-        return Typesofpuja.objects.all()
+        return Typesofpuja.objects.filter()
 
     def lastmod(self, obj):
         return obj.updated_at
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['home']
+
+    def location(self, item):
+        return reverse(item)
